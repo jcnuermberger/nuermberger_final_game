f6d94c84286a6fc361d5ee9bfbb16b02c1839878
jcnuermberger <m264686@usna.edu>
2023-11-29 13:38:47 -0500

    Update targets.py

--- targets.py
+++ targets.py
@@ -17,21 +17,3 @@ class Bottle(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 bottles = pygame.sprite.Group()
 
-
-class Fish(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.image.load("../assets/sprites/green_fish.png").convert()
-        self.image = pygame.transform.flip(self.image, True, False)
-        self.image.set_colorkey((0,0,0))
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-        self.rect.center = (x,y)
-        # self.speed = random.uniform(MIN_SPEED,MAX_SPEED)
-    def update(self):
-        self.x -= self.speed
-        self.rect.x = self.x
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-fishes = pygame.sprite.Group()
